1702b86c7dec4a83d5dd0f59f75c43304d2e39ae
David Pflug <david@tpflug.com>
2018-05-11 12:37:47 -0400

    Solve Secret Handshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,3 +1,5 @@
+use Bitwise
+
 defmodule SecretHandshake do
   @doc """
   Determine the actions of a secret handshake based on the binary
@@ -14,6 +16,26 @@ defmodule SecretHandshake do
   10000 = Reverse the order of the operations in the secret handshake
   """
   @spec commands(code :: integer) :: list(String.t())
-  def commands(code) do
+  def commands(code) when (0b11111 &&& code) == 0, do: []
+
+  def commands(code) when (0b10000 &&& code) > 0 do
+    Enum.reverse(commands(code &&& 0b01111))
   end
+
+  def commands(code) when (0b00001 &&& code) > 0 do
+    ["wink"] ++ commands(code &&& 0b11110)
+  end
+
+  def commands(code) when (0b00010 &&& code) > 0 do
+    ["double blink"] ++ commands(code &&& 0b11100)
+  end
+
+  def commands(code) when (0b00100 &&& code) > 0 do
+    ["close your eyes"] ++ commands(code &&& 0b11000)
+  end
+
+  def commands(code) when (0b01000 &&& code) > 0 do
+    ["jump"] ++ commands(code &&& 0b10000)
+  end
+
 end
