932a722fd79879660b5acc169abbf99d7ff202ff
David Pflug <david@pflug.email>
2020-02-15 19:58:58 -0500

    Simplify solution

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -2,15 +2,8 @@
 package gigasecond
 
 import "time"
-import "log"
 
 // AddGigasecond takes a time and add one BILLION seconds. Mwahaha
 func AddGigasecond(t time.Time) time.Time {
-	newtime, err := time.ParseDuration("1000000000s")
-	if err == nil {
-		return t.Add(newtime)
-	} else {
-		log.Fatal(err)
-		return t
-	}
+	return t.Add(1e9 * time.Second)
 }
