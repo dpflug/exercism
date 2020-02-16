55243cc0838a89b821236f32a5b000fb9df4bfcc
David Pflug <david@pflug.email>
2020-02-16 08:55:30 -0500

    Solve rust/pythagorean-triplet

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -2,14 +2,14 @@ pub fn is_leap_year(year: i32) -> bool {
     if year % 4 == 0 {
         if year % 100 == 0 {
             if year % 400 == 0 {
-                return true
+                return true;
             } else {
-                return false
+                return false;
             }
         } else {
-            return true
+            return true;
         }
     } else {
-        return false
+        return false;
     }
 }
