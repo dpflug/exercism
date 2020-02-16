c7619c8b5d38ac87396f66dee88838b1ff82bbd8
David Pflug <david@pflug.email>
2020-02-15 21:36:03 -0500

    Solve go/two-fer

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,19 @@
+// Implements a Exercism.io Two-Fer puzzle solution
+package twofer
+
+import "strings"
+
+// ShareWith shares something with its argument, defaulting to "you"
+func ShareWith(name string) string {
+	var out strings.Builder
+
+	out.WriteString("One for ")
+	if name == "" {
+		out.WriteString("you")
+	} else {
+		out.WriteString(name)
+	}
+	out.WriteString(", one for me.")
+
+	return out.String()
+}
