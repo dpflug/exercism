87be004114cb4fcb7c4e08e65b37184b94028b77
David Pflug <david@pflug.email>
2020-02-15 22:32:51 -0500

    Solve go/scrabble-score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,30 @@
+package scrabble
+
+import "strings"
+
+func Score(s string) int {
+	var score int = 0
+	if s == "" {
+		return 0
+	}
+	sl := strings.ToLower(s)
+	for i := 0; i < len(s); i++ {
+		switch sl[i] {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		}
+	}
+	return score
+}
