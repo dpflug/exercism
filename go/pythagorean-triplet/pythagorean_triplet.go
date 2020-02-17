d75b8c30f57e816b05ab28686fbae27ce99c56d8
David Pflug <david@pflug.email>
2020-02-16 19:54:57 -0500

    solve go/pythagorean-triplet

---
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -0,0 +1,36 @@
+// Working with Pythagorean triplets
+package pythagorean
+
+//import "log"
+
+type Triplet [3]int
+
+// Range finds Pythagorean triplets with side lenghts between 2 bounds
+func Range(min, max int) []Triplet {
+	maxsum := max + max + max - 2
+	var triplets []Triplet
+	for a := min; a < (maxsum / 3); a++ {
+		for b := a + 1; b < (maxsum - 2*a - 1); b++ {
+			for c := b + 1; c < (maxsum - a - b); c++ {
+				if a*a+b*b == c*c {
+					triplets = append(triplets, [3]int{a, b, c})
+				}
+			}
+		}
+	}
+	return triplets
+}
+
+// Sum finds Pythagorean triplets whose side lengths sum to give number
+func Sum(p int) []Triplet {
+	var triplets []Triplet
+	for a := 1; a < p/3; a++ {
+		for b := a + 1; b < p-2*a-1; b++ {
+			c := p - b - a
+			if a*a+b*b == c*c && a+b+c == p {
+				triplets = append(triplets, [3]int{a, b, c})
+			}
+		}
+	}
+	return triplets
+}
