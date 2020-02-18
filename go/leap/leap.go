06e23198e30c0e8c48913bb006ebf33f7274447a
David Pflug <david@pflug.email>
2020-02-17 19:31:01 -0500

    Simplify go/leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,5 +3,5 @@ package leap
 
 // IsLeapYear does what it says on the tin
 func IsLeapYear(i int) bool {
-	return i%400 == 0 || i%4 == 0 && !(i%100 == 0)
+	return i%400 == 0 || i%4 == 0 && i%100 != 0
 }
