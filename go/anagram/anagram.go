7a9a7365b700fdb892ef57712c98e54f57d791cf
David Pflug <david@pflug.io>
2021-03-28 23:22:05 -0400

    Optimize go/anagram

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -10,7 +10,7 @@ func Detect(subject string, candidates []string) []string {
 	var results = []string{}
 	var sub_chars = make(map[rune]int)
 	for _, char := range subject {
-		sub_chars[unicode.ToLower(char)]++
+		sub_chars[unicode.ToLower(char)] += 1
 	}
 	for _, candidate := range candidates {
 		var cand_chars = make(map[rune]int)
@@ -18,7 +18,7 @@ func Detect(subject string, candidates []string) []string {
 			continue
 		}
 		for _, char := range candidate {
-			cand_chars[unicode.ToLower(char)]++
+			cand_chars[unicode.ToLower(char)] += 1
 		}
 		if reflect.DeepEqual(sub_chars, cand_chars) {
 			results = append(results, candidate)
