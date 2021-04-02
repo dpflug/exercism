1706b2e1b104745205777073c2a7daecbd6be2cf
David Pflug <david@pflug.io>
2021-04-02 15:28:28 -0400

    Improve rust/clock
    Based on advice from mentor "bobahop"

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -5,23 +5,16 @@ pub struct Clock {
     minutes: i32,
 }
 
+const MAX_MINUTES: i32 = 60;
+const MAX_HOURS: i32 = 24;
+
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         // Rust doesn't have a real modulo, so this is what we're stuck with.
-        println!("Ninutes: {}", minutes);
-        let new_minutes = ((minutes % 60) + 60) % 60;
-        println!("New minutes: {}", new_minutes);
-        let minutes_carry: i32;
-        if minutes < 0 {
-            minutes_carry = (minutes + 1) / 60 - 1;
-        } else {
-            minutes_carry = minutes / 60;
-        }
-        println!("Minutes carry: {}", minutes_carry);
+        let new_minutes = minutes.rem_euclid(MAX_MINUTES);
+        let minutes_carry = minutes.div_euclid(MAX_MINUTES);
         let hours_minuted = hours + minutes_carry;
-        println!("Hours minuted: {}", hours_minuted);
-        let new_hours = (hours_minuted % 24 + 24) % 24;
-        println!("New hours: {}", new_hours);
+        let new_hours = hours_minuted.rem_euclid(MAX_HOURS);
         Clock {hours: new_hours, minutes: new_minutes}
     }
 
